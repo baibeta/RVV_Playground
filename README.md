fc5d08bee27541adac264fb14997fd28960d915b
yifan.bai <yifan.bai@hexintek.com>
2023-03-07 11:27:05 +0800

    Try load with mask.

--- README.md
+++ README.md
@@ -7,3 +7,31 @@ THead gcc&gdb:
 
 THead QEMU:
 >https://occ.t-head.cn/community/download?id=4108987827910414336
+
+THead RVV Intrinsic Manual:
+>https://occ-oss-prod.oss-cn-hangzhou.aliyuncs.com/resource//1663142200670/Xuantie+900+Series+RVV-1.0+Intrinsic+Manual.pdf
+
+# DEBUG
+Run QEMU and pause to debug
+```
+qemu-riscv64 -cpu c908v -g 1234 rvv_ld_st
+```
+Run GDB and attach to QEMU
+```
+riscv64-unknown-linux-gnu-gdb rvv_demo -ex "target remote localhost:1234"
+```
+eg. print vector register:
+```
+(gdb) c
+Continuing.
+
+Breakpoint 2, main () at rvv_ld_st.c:14
+14          printf("b = [%f %f %f %f]\n",b[0],b[1],b[2],b[3]);
+(gdb) p/x $v1
+$1 = {q = {0x3ecccccd3e99999a3e4ccccd3dcccccd},
+      l = {0x3e4ccccd3dcccccd, 0x3ecccccd3e99999a},
+      w = {0x3dcccccd, 0x3e4ccccd, 0x3e99999a, 0x3ecccccd},
+      s = {0xcccd, 0x3dcc, 0xcccd, 0x3e4c, 0x999a, 0x3e99, 0xcccd, 0x3ecc},
+      b = {0xcd, 0xcc, 0xcc, 0x3d, 0xcd, 0xcc, 0x4c, 0x3e, 0x9a, 0x99, 0x99, 0x3e, 0xcd, 0xcc, 0xcc, 0x3e}}
+```
+
--- rvv_ld_st.c
+++ rvv_ld_st.c
@@ -5,12 +5,54 @@ int main() {
     float a[] = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8};
     float b[] = {0, 0, 0, 0};
 
+    printf("Test basic load and store function.");
     vfloat32m1_t v_a;
     v_a = vle32_v_f32m1 (a, 4);
-
     vse32_v_f32m1(b, v_a, 2);
-
     // b = [0.100000 0.200000 0.000000 0.000000]
+    printf("b = [%f %f %f %f]\n",b[0],b[1],b[2],b[3]);
+
+
+    printf("Test load and store function with LMUL.");
+    vfloat32mf2_t v_aa;
+    // mf2 means LMUL = 1/2
+    // max vl = vlen * lmul / sew = 128 * 0.5 / 32 = 2
+    // so v_aa = [0.1, 0.2]
+    v_aa = vle32_v_f32mf2 (a, 4);
+    vse32_v_f32mf2(b, v_aa, 4);
+    // b = [0.100000 0.200000 0.000000 0.000000]
+    printf("b = [%f %f %f %f]\n",b[0],b[1],b[2],b[3]);
+
+
+    printf("Test VL.");
+    // check vl
+    size_t vlmax = vsetvlmax_e32mf2();
+    printf("lmul = 1/2, vlmax = %d \n",vlmax);
+    // vlmax = 2
+    // vl <= vlmax && vl <= avl
+    size_t vl = vsetvl_e32mf2(4);
+    printf("lmul = 1/2, avl = 4, vl = %d \n",vl);
+    // avl = 1
+    vl = vsetvl_e32mf2(1);
+    printf("lmul = 1/2, avl = 1, vl = %d \n",vl);
+
+
+    printf("Test load function with Mask.");
+    // vbooln_t => (n = SEW/LMUL = 32/1 = 32) => vbool32_t
+    vbool32_t _mask;
+    // Vector Floating-Point Less Than
+    // v_a = [0.1 0.2 0.3 0.4]
+    // _mask => |v_a < 0.4| => _mask = [1 1 1 0]
+    float mask_threshold = 0.4f;
+    _mask = vmflt_vf_f32m1_b32(v_a, mask_threshold, 4);
+    // maskedoff = c
+    float c[] = {-0.1, -0.2, -0.3, -0.4};
+    vfloat32m1_t masked_v_a, v_maskedoff;
+    v_maskedoff = vle32_v_f32m1(c, 4);
+    // masked_v_a = a[0] a[1] a[2] c[3]
+    masked_v_a = vle32_v_f32m1_m(_mask, v_maskedoff, a, 4);
+    vse32_v_f32m1(b, masked_v_a, 4);
+    // b = [0.100000 0.200000 0.300000 -0.400000]
     printf("b = [%f %f %f %f]\n",b[0],b[1],b[2],b[3]);
 
     return 0;
