3aeaadd659bb12bf45c60182a2bf3e47ccc7b3e4
yifan.bai <yifan.bai@hexintek.com>
2023-03-07 15:44:25 +0800

    test strided and indexed load store.

--- rvv_ld_st.c
+++ rvv_ld_st.c
@@ -1,11 +1,103 @@
 #include <riscv_vector.h>
 #include <stdio.h>
 
+
+void test_strided() {
+    printf("Test strided load and store. \n");
+    // load stride = 1 dst = [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16]
+    // load stride = 2 dst = [1 3 5 7 9 11 13 15]
+    // load stride = 4 dst = [1 5 9 13]
+    // store stride = 1 dst = [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16]
+    // store stride = 2 dst = [1 0 2 0 3 0 4 0 5 0 6 0 7 0 8 0]
+    // store stride = 4 dst = [1 0 0 0 2 0 0 0 3 0 0 0 4 0 0 0]
+    int8_t src[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+    int8_t dst[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+    ptrdiff_t stride = 1 * sizeof(int8_t);
+    vint8m1_t v_i8 = vlse8_v_i8m1(src, stride, 16);
+    vse8_v_i8m1(dst, v_i8, 16);
+    printf("load stride = 1 dst = [%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d]\n",
+                    dst[0],dst[1],dst[2],dst[3],
+                    dst[4],dst[5],dst[6],dst[7],
+                    dst[8],dst[9],dst[10],dst[11],
+                    dst[12],dst[13],dst[14],dst[15]);
+
+    stride = 2 * sizeof(int8_t);
+    v_i8 = vlse8_v_i8m1(src, stride, 8);
+    vse8_v_i8m1(dst, v_i8, 8);
+    printf("load stride = 2 dst = [%d %d %d %d %d %d %d %d]\n",
+                    dst[0],dst[1],dst[2],dst[3],dst[4],dst[5],dst[6],dst[7]);
+
+    stride = 4 * sizeof(int8_t);
+    v_i8 = vlse8_v_i8m1(src, stride, 4);
+    vse8_v_i8m1(dst, v_i8, 4);
+    printf("load stride = 4 dst = [%d %d %d %d]\n",dst[0],dst[1],dst[2],dst[3]);
+
+
+    stride = 1 * sizeof(int8_t);
+    v_i8 = vle8_v_i8m1(src, 16);
+    vsse8_v_i8m1(dst, stride, v_i8, 16);
+    printf("store stride = 1 dst = [%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d]\n",
+                    dst[0],dst[1],dst[2],dst[3],
+                    dst[4],dst[5],dst[6],dst[7],
+                    dst[8],dst[9],dst[10],dst[11],
+                    dst[12],dst[13],dst[14],dst[15]);
+
+    stride = 2 * sizeof(int8_t);
+    memset(dst, 0, sizeof(dst));
+    vsse8_v_i8m1(dst, stride, v_i8, 8);
+    printf("store stride = 2 dst = [%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d]\n",
+                    dst[0],dst[1],dst[2],dst[3],
+                    dst[4],dst[5],dst[6],dst[7],
+                    dst[8],dst[9],dst[10],dst[11],
+                    dst[12],dst[13],dst[14],dst[15]);
+
+    stride = 4 * sizeof(int8_t);
+    memset(dst, 0, sizeof(dst));
+    vsse8_v_i8m1(dst, stride, v_i8, 4);
+    printf("store stride = 4 dst = [%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d]\n",
+                    dst[0],dst[1],dst[2],dst[3],
+                    dst[4],dst[5],dst[6],dst[7],
+                    dst[8],dst[9],dst[10],dst[11],
+                    dst[12],dst[13],dst[14],dst[15]);
+}
+
+
+
+void test_indexed() {
+    printf("Test indexed load and store. \n");
+    int8_t src[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+    int8_t dst[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    int8_t index[] = {1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4};
+
+    vuint8m1_t bindex = vle8_v_u8m1(index, 16);
+    vint8m1_t v_dst = vloxei8_v_i8m1(src, bindex, 16);
+
+    vse8_v_i8m1(dst, v_dst, 16);
+    // indexed load dst = [2 3 4 5 2 3 4 5 2 3 4 5 2 3 4 5]
+    printf("indexed load dst = [%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d]\n",
+                    dst[0],dst[1],dst[2],dst[3],
+                    dst[4],dst[5],dst[6],dst[7],
+                    dst[8],dst[9],dst[10],dst[11],
+                    dst[12],dst[13],dst[14],dst[15]);
+
+    memset(dst, 0, sizeof(dst));
+    v_dst = vluxei8_v_i8m1(src, bindex, 16);
+    vse8_v_i8m1(dst, v_dst, 16);
+    // indexed load dst = [2 3 4 5 2 3 4 5 2 3 4 5 2 3 4 5]
+    printf("indexed load dst = [%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d]\n",
+                    dst[0],dst[1],dst[2],dst[3],
+                    dst[4],dst[5],dst[6],dst[7],
+                    dst[8],dst[9],dst[10],dst[11],
+                    dst[12],dst[13],dst[14],dst[15]);
+}
+
+
 int main() {
     float a[] = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8};
     float b[] = {0, 0, 0, 0};
 
-    printf("Test basic load and store function.");
+    printf("Test basic load and store function.\n");
     vfloat32m1_t v_a;
     v_a = vle32_v_f32m1 (a, 4);
     vse32_v_f32m1(b, v_a, 2);
@@ -13,7 +105,7 @@ int main() {
     printf("b = [%f %f %f %f]\n",b[0],b[1],b[2],b[3]);
 
 
-    printf("Test load and store function with LMUL.");
+    printf("Test load and store function with LMUL.\n");
     vfloat32mf2_t v_aa;
     // mf2 means LMUL = 1/2
     // max vl = vlen * lmul / sew = 128 * 0.5 / 32 = 2
@@ -24,7 +116,7 @@ int main() {
     printf("b = [%f %f %f %f]\n",b[0],b[1],b[2],b[3]);
 
 
-    printf("Test VL.");
+    printf("Test VL.\n");
     // check vl
     size_t vlmax = vsetvlmax_e32mf2();
     printf("lmul = 1/2, vlmax = %d \n",vlmax);
@@ -37,7 +129,7 @@ int main() {
     printf("lmul = 1/2, avl = 1, vl = %d \n",vl);
 
 
-    printf("Test load function with Mask.");
+    printf("Test load function with Mask.\n");
     // vbooln_t => (n = SEW/LMUL = 32/1 = 32) => vbool32_t
     vbool32_t _mask;
     // Vector Floating-Point Less Than
@@ -54,6 +146,12 @@ int main() {
     vse32_v_f32m1(b, masked_v_a, 4);
     // b = [0.100000 0.200000 0.300000 -0.400000]
     printf("b = [%f %f %f %f]\n",b[0],b[1],b[2],b[3]);
+
+
+    test_strided();
+
+    test_indexed();
+    printf("Finish.\n");
 
     return 0;
 }
